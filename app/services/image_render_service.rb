78f505ad9c1bf0b3f05c90da078d3110d66a3cb9
Hoang Tuan <hoangtuanictvn@gmail.com>
2019-12-26 11:32:05 +0900

    Deploy 2.4.9

--- app/services/image_render_service.rb
+++ app/services/image_render_service.rb
@@ -1,17 +1,18 @@
 class ImageRenderService
-    def self.render card, user_id, uploaded=true
+    def self.render card, uploaded=true
         Cloudinary::Uploader.destroy(card.code) unless card.code.nil?
-        Dir.mkdir("#{Rails.root}/storage") unless File.exists?("#{Rails.root}/storage")
-        file = File.open("./storage/#{card.code}}.png", "wb") do|f|
+        path = File.join Rails.root, "storage"
+        File.mkdir(path) unless File.exist?(path) 
+        file = File.open(File.join(path, "#{card.code}.png"), "wb") do|f|
             f.write(Base64.decode64(card.thumb.gsub('data:image/png;base64,','')))
         end
-        if uploaded
-            res = Cloudinary::Uploader.upload("./storage/#{card.code}.png", {
+        if uploaded == true
+            res = Cloudinary::Uploader.upload(File.join(path, "#{card.code}.png"), {
                 public_id: card.code
             })
-            File.delete("./storage/#{card.code}.png");
+            File.delete(File.join(path, "#{card.code}.png"));
             return card.code, res['secure_url']
         end
-        return "./storage/#{card.code}.png"
+        return File.join(path, "#{card.code}.png")
     end
 end
